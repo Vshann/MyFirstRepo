8d4d072893d59c8640f440332d67cf3d62c37a79
Vshann <33519452+Vshann@users.noreply.github.com>
2017-11-09 09:13:18 -0500

    initial

---
+++ madlib_VS.py
@@ -0,0 +1,63 @@
+### MAD LIBS ###
+### Source: http://nblsb.weebly.com/uploads/2/6/6/2/26626157/6679913_orig.jpg
+
+print("write a plural noun.")
+pluralnoun1 = input()
+
+print("write an adverb.")
+adverb1 = input()
+
+print("write a verb.")
+verb1 = input()
+
+print("article of clothing.")
+articleofclothing1 = input()
+
+print("body part.")
+bodypart1 = input()
+
+print("adjective.")
+adjective1 = input()
+
+print("noun.")
+noun1 = input()
+
+print("write a plural noun.")
+pluralnoun2 = input()
+
+print("body part.")
+bodypart2 = input ()
+
+print("write a plural noun.")
+pluralnoun3 = input()
+
+print("body part.")
+bodypart3 = input ()
+
+print("noun.")
+noun2 = input()
+
+print("noun.")
+noun3 = input()
+
+print ("write verb ending in ing.")
+verbendingining1 = input
+
+print("adjective.")
+adjective2 = input()
+
+print("adjective.")
+adjective2 = input()
+
+print("write a verb.")
+verb2 = input()
+
+
+
+
+
+
+
+
+
+
